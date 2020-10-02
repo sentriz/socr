bcee84e74a37c16456ffcbccaa048f2add1141b8
sentriz <senan@senan.xyz>
2020-10-02 18:47:56 +0100

    handle image 404s a bit cleaner

--- frontend/src/components/Public.vue
+++ frontend/src/components/Public.vue
@@ -1,13 +1,11 @@
 <!-- <ScreenshotHighlight v-if="screenshot" :screenshot="screenshot" /> -->
 <template>
   <div class="bg-gray-200 min-h-screen">
-    <div v-show="screenshot" class="container mx-auto p-8 space-y-4">
-      <div class="border-2 border-solid border-white bg-black shadow min-h-2">
-        <img class="mx-auto" :src="url" />
+    <div v-show="imageHave" class="container mx-auto p-8 space-y-4">
+      <div class="box bg-black">
+        <img class="mx-auto" :src="imageSrc" @load="imageLoaded" />
       </div>
-      <div
-        class="border-2 border-solid border-white shadow bg-gray-300 padded font-mono text-sm"
-      >
+      <div class="box bg-gray-300 padded font-mono text-sm">
         <p v-show="text.length == 0" class="text-gray-600 py-2">
           <i class="animate-spin fas fa-circle-notch"></i> processing screenshot...
         </p>
@@ -33,20 +31,22 @@ const router = useRouter();
 const route = useRoute();
 const screenshotID = route.params.id;
 
-export const url = `${urlScreenshot}/${screenshotID}/raw`;
 export const screenshot = ref(null);
 export const text = ref([]);
-
 const requestScreenshot = async () => {
   const resp = await reqScreenshot(screenshotID);
-  if (resp.hits.length == 0) {
-    router.replace({ name: "not_found" });
-    return;
-  }
+  if (resp.hits.length == 0) return;
 
   const hit = resp.hits[0];
   screenshot.value = hit;
   text.value = hit.fields[fields.BLOCKS_TEXT];
+};
+
+// suspend showing anything until we have an image
+export const imageSrc = `${urlScreenshot}/${screenshotID}/raw`;
+export const imageHave = ref(false);
+export const imageLoaded = () => {
+  imageHave.value = true;
 };
 
 // fetch image on mount
