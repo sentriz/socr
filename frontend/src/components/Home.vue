27b1ef8a54fae5fa59a3b6a1c0e2ffb683310907
sentriz <senan@senan.xyz>
2020-10-05 21:58:38 +0100

    space out nav links

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -3,17 +3,17 @@
     <ul class="flex my-3">
       <div class="flex-1 space-x-6">
         <router-link class="text-gray-500 hover:text-blue-300" :to="{ name: 'search' }">
-          <i class="fas fa-search"></i>
-          search
+          <i class="fas fa-search mr-2"></i>
+          <span>search</span>
         </router-link>
         <router-link class="text-gray-500 hover:text-blue-300" :to="{ name: 'settings' }">
-          <i class="fas fa-cogs"></i>
-          settings
+          <i class="fas fa-cogs mr-2"></i>
+          <span>settings</span>
         </router-link>
       </div>
       <router-link class="text-gray-500 hover:text-blue-300" :to="{ name: 'logout' }">
-        <i class="fas fa-sign-out-alt"></i>
-        logout
+        <i class="fas fa-sign-out-alt mr-2"></i>
+        <span>logout</span>
       </router-link>
     </ul>
     <hr class="mt-0 mb-6" />
