6b8639a3e28b49e2599678380eb03f6ad1f67229
sentriz <senan@senan.xyz>
2020-10-06 20:21:57 +0100

    clean up sidebar animation stuff

--- frontend/src/components/SearchSidebar.vue
+++ frontend/src/components/SearchSidebar.vue
@@ -1,15 +1,15 @@
 <template>
   <Transition
-    enterFromClass="translate-x-full"
     enterActiveClass="transform transition ease-in-out duration-200"
     enterToClass="translate-x-0"
-    leaveFromClass="translate-x-0"
+    enterFromClass="translate-x-full"
     leaveActiveClass="transform transition ease-in-out duration-200"
     leaveToClass="translate-x-full"
+    leaveFromClass="translate-x-0"
   >
     <div
       v-if="screenshot"
-      class="fixed inset-0 w-9/12 ml-auto p-6 bg-white overflow-y-auto z-20"
+      class="z-20 fixed inset-y-0 right-0 w-9/12 p-6 bg-white overflow-y-auto"
     >
       <div class="mx-auto space-y-6">
         <div class="bg-black shadow">
@@ -24,16 +24,16 @@
     </div>
   </Transition>
   <Transition
-    enterFromClass="opacity-0"
     enterActiveClass="ease-in-out duration-500"
     enterToClass="opacity-100"
-    leaveFromClass="opacity-100"
+    enterFromClass="opacity-0"
     leaveActiveClass="ease-in-out duration-500"
     leaveToClass="opacity-0"
+    leaveFromClass="opacity-100"
   >
     <div
       v-if="screenshot"
-      class="fixed inset-0 bg-gray-700 bg-opacity-50 transition-opacity pointer-events-none"
+      class="z-10 fixed inset-0 bg-gray-700 bg-opacity-50 transition-opacity pointer-events-none"
     />
   </Transition>
 </template>
