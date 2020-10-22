fdb26672b448cfee528aa23e9b525ac933e5039b
sentriz <senan@senan.xyz>
2020-10-22 22:08:10 +0100

    add hidden index package

---
+++ backend/index/index.go
@@ -0,0 +1,64 @@
+package index
+
+import (
+	"errors"
+	"fmt"
+
+	"github.com/blevesearch/bleve"
+	"github.com/blevesearch/bleve/analysis/analyzer/keyword"
+	"github.com/blevesearch/bleve/mapping"
+)
+
+var (
+	BaseSearchFields = []string{
+		"tags",
+		"timestamp",
+		"blocks.text",
+		"blocks.position",
+		"dimensions.height",
+		"dimensions.width",
+	}
+	BaseHighlightFields = []string{
+		"blocks.text",
+	}
+	BaseQueryField = "blocks.text"
+)
+
+func CreateIndexMapping() *mapping.IndexMappingImpl {
+	fieldMapNumeric := bleve.NewNumericFieldMapping()
+
+	fieldMapKeyword := bleve.NewTextFieldMapping()
+	fieldMapKeyword.Analyzer = keyword.Name
+
+	fieldMapTime := bleve.NewDateTimeFieldMapping()
+
+	mappingBlocks := bleve.NewDocumentStaticMapping()
+	mappingBlocks.AddFieldMappingsAt("text", fieldMapKeyword)
+	mappingBlocks.AddFieldMappingsAt("position", fieldMapNumeric)
+
+	mappingScreenshot := bleve.NewDocumentStaticMapping()
+	mappingScreenshot.AddFieldMappingsAt("timestamp", fieldMapTime)
+	mappingScreenshot.AddFieldMappingsAt("tags", fieldMapKeyword)
+	mappingScreenshot.AddSubDocumentMapping("blocks", mappingBlocks)
+
+	mappingIndex := bleve.NewIndexMapping()
+	mappingIndex.DefaultMapping = mappingScreenshot
+	mappingIndex.DefaultField = BaseQueryField
+
+	return mappingIndex
+}
+
+func GetOrCreateIndex(path string) (bleve.Index, error) {
+	index, err := bleve.Open(path)
+	switch {
+	case
+		errors.Is(err, bleve.ErrorIndexMetaMissing),
+		errors.Is(err, bleve.ErrorIndexPathDoesNotExist):
+		indexMapping := CreateIndexMapping()
+		return bleve.New(path, indexMapping)
+	case err != nil:
+		return nil, fmt.Errorf("open index: %w", err)
+	default:
+		return index, nil
+	}
+}
