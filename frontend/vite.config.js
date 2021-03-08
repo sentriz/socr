9a7c16bb873eff1220778a503ee39743d732cabd
sentriz <senan@senan.xyz>
2021-03-08 19:11:52 +0000

    update frontend deps

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,5 +1,7 @@
-module.exports = {
-  optimizeDeps: {
-    exclude: ["postcss"]
-  }
-}
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()]
+})
