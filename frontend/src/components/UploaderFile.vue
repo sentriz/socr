db4b556d161c7ffc3b30829dbecbbb85b878a23a
sentriz <senan@senan.xyz>
2021-03-26 19:24:06 +0000

    reword UploaderFile

--- frontend/src/components/UploaderFile.vue
+++ frontend/src/components/UploaderFile.vue
@@ -3,7 +3,7 @@
   <div class="fixed bottom-0 opacity-75 hover:opacity-100 right-0 padded bg-gray-200 rounded-tl">
     <div class="flex items-center gap-2 text-gray-600 hover:text-gray-900 hover:cursor-pointer" @click="click">
       <i class="fas fa-upload text-sm"></i>
-      <span>select file to upload or just paste</span>
+      <span>upload or paste file</span>
     </div>
   </div>
   <LoadingModal :loading="loading" text="uploading from file" />
