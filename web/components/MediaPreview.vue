51a56f8ed445e406d7224538e15af063199ac581
sentriz <senan@senan.xyz>
2022-12-07 23:18:48 +0000

    fix(ui): fix ts build

--- web/components/MediaPreview.vue
+++ web/components/MediaPreview.vue
@@ -5,7 +5,7 @@
   <loading-spinner v-else class="bg-gray-100" text="processing image" />
 
   <div v-if="!isVideo && blocks.length" class="box padded bg-gray-100 font-mono text-sm">
-    <p v-for="(block, i) in blocks" :key="i" class="overflow-x-hidden rounded-lg" :class="{ 'bg-yellow-200/90': highlightedBlocksIndexes.has(i) }">{{ block.body }}</p>
+    <p v-for="(block, i) in blocks" :key="i" class="rounded-lg overflow-x-hidden" :class="{ 'bg-yellow-200/90': highlightedBlocksIndexes.has(i) }">{{ block.body }}</p>
   </div>
   <loading-spinner v-if="!isVideo && !media?.processed" class="bg-gray-100" text="processing text" />
 </template>
--- web/components/NavItem.vue
+++ web/components/NavItem.vue
@@ -1,6 +1,8 @@
 <template>
-  <router-link class="flex h-5 items-center space-x-2 text-gray-500 hover:text-blue-400" active-class="text-blue-500">
+  <router-link :to="`${$attrs.to}`" class="flex h-5 items-center space-x-2 text-gray-500 hover:text-blue-400" active-class="text-blue-500">
     <slot name="icon"></slot>
-    <span><slot name="name"></slot></span>
+    <span>
+      <slot name="name"></slot>
+    </span>
   </router-link>
 </template>
