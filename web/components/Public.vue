1e15eaf952270f71777f631a47735394d52fe76d
sentriz <senan@senan.xyz>
2022-12-08 00:00:52 +0000

    feat(ui): update public page styling

--- web/components/Public.vue
+++ web/components/Public.vue
@@ -2,8 +2,8 @@
   <div class="flex min-h-screen items-center bg-white">
     <div class="container mx-auto space-y-6 p-6">
       <div v-if="media" class="flex items-center justify-between">
-        <h1 class="text-gray-500">shared media</h1>
-        <badge-group label="uploaded" class="hidden text-gray-700 md:inline-flex" v-if="media && timestamp">
+        <h1 class="text-gray-700">shared media</h1>
+        <badge-group label="created on" class="hidden text-gray-500 md:inline-flex" v-if="media && timestamp">
           <badge class="bg-pink-200 text-pink-900" :title="media.timestamp">{{ timestamp }}</badge>
         </badge-group>
       </div>
@@ -32,8 +32,11 @@ const requestMedia = async () => {
 const media = computed(() => store.getMediaByHash(hash))
 const timestamp = computed(() => {
   if (!media.value) return
-  return new Date(media.value?.timestamp).toLocaleString()
+  const date = new Date(media.value?.timestamp)
+  return `${pad(date.getFullYear())}.${pad(date.getMonth())}.${pad(date.getDay())}`
 })
+
+const pad = (n: number): string => String(n).padStart(2, '0')
 
 onMounted(requestMedia)
 
